64bcba9bb48600a67c183d4f8cf4abea896f364b
Izabelly Coser Guindani <460498@unoesc.lan>
2026-08-05 20:18:15 -0300

    commit

---
+++ Vetores/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.WriteLine("Hello, World!");
+
+// comentário de uma linha 
+/*
+comentário de múltiplas linhas
+*/
+
+// alguns tipos de dados
+int number = 10;
+string name = "izabelly coser guindani";
+double quantity = 0.0;
+bool hasMoney = true;
+char letter = 'A';
+
+//constantes (sempre em letra maiúscula)
+const int DAYS_IN_WEEK = 7;
+
+letter = 'B';
+
+Console.WriteLine("Informe seu nome: ");
+name = Console.ReadLine();
+
+Console.Write("Seja bem vindo "+ name);
+
+// enumeradores
+
+//enum language {PT, EN, ES}
+
+// concatenando strings
+
+string firstName = "Izabelly", lastName ="";
+lastName = "Coser";
+
+string fullName = firstName + " " + lastName;
+Console.WriteLine (fullName);
+
+/*
+lowercase
+UPPERCASE
+snake_case
+PascalCase
+camelCase
+*/
