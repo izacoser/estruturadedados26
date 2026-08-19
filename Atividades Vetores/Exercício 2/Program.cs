221a1eb59d880c7478a383e4aa8f092761833e45
izacoser <izabellycoser@gmail.com>
2026-08-19 16:01:36 -0300

    Exercícios vetores

---
+++ Atividades Vetores/Exercício 2/Program.cs
@@ -0,0 +1,36 @@
+﻿/*ESCREVA UM PROGRAMA QUE LEIA UMA PALAVRA DIGITADA PELO USUÁRIO E IDENTIFIQUE SE A PALAVRA É OU NÃO UM PALÍNDROMO (OVO, MUSSUM, ARARA)*/
+
+Console.WriteLine("Escreva uma palavra: ");
+string palavra = Console.ReadLine();
+
+int quantletras = palavra.Length;
+int[] Array = new int[quantletras];
+int[] invertida = new int[quantletras];
+int palindromo = 1; 
+
+for (int i = 0; i < quantletras; i++)
+{
+    Array[i] = (int)palavra[i];
+}
+
+for (int i = 0; i < quantletras; i++)
+{
+    invertida[i] = Array[quantletras - 1 - i];
+}
+
+for (int i = 0; i < quantletras; i++)
+{
+    if (Array[i] != invertida[i])
+    {
+        palindromo = 0;
+    }
+}
+
+if (palindromo == 1)
+{
+    Console.WriteLine("É um palíndromo");
+}
+else
+{
+    Console.WriteLine("Não é um palíndromo");
+}
